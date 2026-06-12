e12be6d6134e11950c491199ad72b9c2ba2ceef6
Nandini p <pulinandinireddy03@gmail.com>
2026-06-12 14:18:25 +0530

    Initial Commit

---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.entity.Employee;
+import com.example.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return service.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return service.getAllEmployees();
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id,
+                                   @RequestBody Employee employee) {
+        return service.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+
+        service.deleteEmployee(id);
+
+        return "Employee Deleted Successfully";
+    }
+}
---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,43 @@
+package com.example.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Employee;
+import com.example.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    public Employee saveEmployee(Employee employee) {
+        return repository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return repository.findAll();
+    }
+
+    public Employee updateEmployee(Long id, Employee employee) {
+
+        Employee existingEmployee = repository.findById(id).orElse(null);
+
+        if (existingEmployee != null) {
+            existingEmployee.setName(employee.getName());
+            existingEmployee.setEmail(employee.getEmail());
+            existingEmployee.setDepartment(employee.getDepartment());
+            existingEmployee.setSalary(employee.getSalary());
+
+            return repository.save(existingEmployee);
+        }
+
+        return null;
+    }
+
+    public void deleteEmployee(Long id) {
+        repository.deleteById(id);
+    }
+}
